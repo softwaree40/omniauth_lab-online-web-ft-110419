d47c03c07aac129a0b8132121383695bb6bdfc9b
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-25 22:27:25 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # Add your routes here
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  root welcomehome
+  root 'welcome#home'
 end
