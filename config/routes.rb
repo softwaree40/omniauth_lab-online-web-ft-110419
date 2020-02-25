51991322f0c76b6d6fab18af90a834bd4c8faf17
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-25 22:48:15 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # Add your routes here
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match '/auth/github/callback', to: 'sessions#create', via: [:get, :post]
   root 'welcome#home'
 end
